ba86fa6eb3b1a518922f9e679775558f246af5c1
Syldys <syldysnya@gmail.com>
2021-03-15 22:30:45 -0400

    Add errors Homework

---
+++ W4D2/skeleton/super_useful.rb
@@ -0,0 +1,50 @@
+# PHASE 2
+def convert_to_int(str)
+  Integer(str)
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  else
+    raise StandardError.new "Cannot take #{maybe_fruit}, try #{FRUITS.sample}"
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+  maybe_fruit = gets.chomp
+  reaction(maybe_fruit) 
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError.new "Fill info" if name.length <= 0 || fav_pastime.length <= 0
+    raise StandardError.new 'Know each other less than 5 years' if yrs_known >= 5
+
+    @name = name
+    @fav_pastime = fav_pastime
+    @yrs_known = yrs_known
+    
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
