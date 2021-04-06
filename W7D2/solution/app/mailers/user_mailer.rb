2efebf873e23a2e08c5aef74dfb8c5432ccf47dd
syldysnya <syldysnya@gmail.com>
2021-04-06 00:11:55 -0400

    HW for W7D2

---
+++ W7D2/solution/app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+
+    default from: "everybody@appacademy.io"
+
+    def welcome_email(user)
+        @user = user
+        @url = 'http://localhost:3000/session/new'
+        mail(to: user.username, subject: "Welcome to pur puradise!")
+    end
+end
